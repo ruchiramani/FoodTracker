70fb3b926011dcdcae22053817766527f99d5849
ruchi <ruchipatel@Aalaps-Air.home>
2017-03-09 21:34:59 -0500

    added ingredients

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,18 +2,44 @@
 
 //   https://api.nutritionix.com/v1_1/search/cheddar%20cheese?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=[YOURID]&appKey=[YOURKEY]
 $( document ).ready(function(){
-  $( "#ingredient" ).submit(function(e){
+  $( "#show" ).submit(function(e){
     e.preventDefault();
+    e.stopPropagation();
     $.ajax({
       url: $(this).attr('action'),
       type: "POST",
-      data: (this).serialize(),
-      success: function(json) {
-        console.log('here')
+      data: $(this).serialize(),
+      success: function(response){
+        parseResponse(response)
+        console.log(response)
       },
       error: function(xhr){
      var errors = $.parseJSON(xhr.responseText).errors
-   }
+     }
     })
+
   });
+
+
+  function parseResponse(response){
+    response.forEach(function(item){
+      $("#ingredients").append(`<div> <p> ${item.name} -  ${item.calories} </p>  <button class="add" id="${item.id}"> Add </button> </div>` )
+    })
+    $('.add').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $.ajax({
+        url: 'recipe_ingredient/create',
+        type: "POST",
+        data: $(this).serialize(),
+        success: function(response){
+
+        },
+        error: function(xhr){
+       var errors = $.parseJSON(xhr.responseText).errors
+       }
+      })
+    });
+  };
+
 });
---
+++ app/controllers/recipe_ingredients_controller.rb
@@ -0,0 +1,10 @@
+class RecipeIngredientsController < ApplicationControlle
+
+  def new
+  end
+
+  def create
+    binding.pry
+  end
+
+end
